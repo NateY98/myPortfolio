dc9628a68cde65fcc0d04e5ee0d7efcc0a6afa81
NateY <nate.yeow@gmail.com>
2021-04-29 20:08:03 -0400

    added more functionality to the hamburger menu

--- script.js
+++ script.js
@@ -2,6 +2,38 @@ const portfolio = {};
 
 portfolio.hamburger = document.querySelector(".hamburger");
 portfolio.nav = document.querySelector(".navContainer");
+portfolio.navItemOne = document.querySelector(".liOne");
+portfolio.navItemTwo = document.querySelector(".liTwo");
+portfolio.navItemThree = document.querySelector(".liThree");
+portfolio.navItemFour = document.querySelector(".liFour");
+
+portfolio.navItemEventOne = () => {
+  portfolio.navItemOne.addEventListener("click", () => {
+    portfolio.hamburger.classList.toggle("clicked");
+    portfolio.nav.classList.toggle("showNav");
+  });
+};
+
+portfolio.navItemEventTwo = () => {
+  portfolio.navItemTwo.addEventListener("click", () => {
+    portfolio.hamburger.classList.toggle("clicked");
+    portfolio.nav.classList.toggle("showNav");
+  });
+};
+
+portfolio.navItemEventThree = () => {
+  portfolio.navItemThree.addEventListener("click", () => {
+    portfolio.hamburger.classList.toggle("clicked");
+    portfolio.nav.classList.toggle("showNav");
+  });
+};
+
+portfolio.navItemEventFour = () => {
+  portfolio.navItemFour.addEventListener("click", () => {
+    portfolio.hamburger.classList.toggle("clicked");
+    portfolio.nav.classList.toggle("showNav");
+  });
+};
 
 portfolio.hamburgerEvent = () => {
   portfolio.hamburger.addEventListener("click", () => {
@@ -12,6 +44,10 @@ portfolio.hamburgerEvent = () => {
 
 portfolio.init = () => {
   portfolio.hamburgerEvent();
+  portfolio.navItemEventOne();
+  portfolio.navItemEventTwo();
+  portfolio.navItemEventThree();
+  portfolio.navItemEventFour();
 };
 
 portfolio.init();
